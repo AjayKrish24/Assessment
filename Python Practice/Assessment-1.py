5eda453abc95271fe15ac32cebbfc9a9f55c5cd6
AjayKrish24 <60135691+AjayKrish24@users.noreply.github.com>
2020-02-19 18:23:46 +0530

    Update Assessment-1.py

--- Python Practice/Assessment-1.py
+++ Python Practice/Assessment-1.py
@@ -1,21 +1,65 @@
+# 1)
+
+import random
+numbers = []
+for i in range(0,10):
+    if i not in numbers:
+        numbers.append(random.randrange(0,50))
+print(numbers)
+number = random.choice(numbers)
+print(number)
+print("3 chances to find the number?  Enter the number you guess : ")
+for x in range(3):
+    guess = int (input ())
+    if x <= 1:
+        if guess == number:
+            print("You guessed correctly")
+            break
+        elif guess<=number+5 and guess>=number-5:
+            print("You are close,try again")
+
+        else:
+            print("Wild guess ")
+    else:
+        if guess==number:
+            print("You guessed correctly ")
+            break
+        else:
+            print("Oops you are out of chances better luck next time")
+   
+#=======================o/p======================================         
+[40, 19, 34, 2, 40, 15, 34, 44, 6, 49]                                                                                                                   
+34                                                                                                                                                       
+3 chances to find the number?  Enter the number you guess :                                                                                              
+33                                                                                                                                                       
+You are close,try again
+22
+Wild guess
+43                                                                                                                                                       
+Oops you are out of chances better luck next time                                                                                                        
+                                                 
+
+*************************************************************************************************** 
+
 # 2)
 
 d={'one':'1','two':'2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9'}
 e={'plus':'+','minus':'-','multily':'*','divide':'/'}
 s=input("Enter the expression: \n")
 l=s.split(" ")
-# print(l)
 for k in d:
     if l[0]==k:
         a=d.get(k)
     if l[2]==k:
         b=d.get(k)
-# print(a,b)
 for key in e:
     if l[1]==key:
         x=a+e.get(key)+b
-       
-print(eval(x))
+b = eval(x)
+print(d[b])
+
+#=======================o/p======================================
+
 
 ***************************************************************************************************
 
@@ -30,7 +74,6 @@ elif num % 3 != 0 and num % 5 == 0:
     print("Dvisible only by 5")
 else:
     print("Not divisible by both 3 or 5")
-
 
 #=======================o/p======================================
 
